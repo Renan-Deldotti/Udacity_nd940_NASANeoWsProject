5f77704b871f59633e36ed23a82568fb033e084d
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2022-06-18 17:58:50 -0300

    update

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -3,18 +3,20 @@ package com.udacity.asteroidradar.main
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-    private val viewModel: MainViewModel by lazy {
+    /*private val viewModel: MainViewModel by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
-    }
+    }*/
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         val binding = FragmentMainBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
