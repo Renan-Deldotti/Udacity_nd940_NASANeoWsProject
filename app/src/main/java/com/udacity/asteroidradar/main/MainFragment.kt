b6df85712927a156d34579851b9b9693679c130e
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2022-06-19 23:06:05 -0300

    Finish adding RecyclerView

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -6,6 +6,8 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
@@ -15,17 +17,29 @@ class MainFragment : Fragment() {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }*/
     private val viewModel: MainViewModel by activityViewModels()
+    private lateinit var asteroidsListAdapter : AsteroidsListAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         val binding = FragmentMainBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
-        binding.asteroidRecycler.adapter = AsteroidsListAdapter(AsteroidsListAdapter.AsteroidsItemListener {
-            Toast.makeText(context, "Test: ${it.id}", Toast.LENGTH_SHORT).show()
+        binding.viewModel = viewModel
+
+        asteroidsListAdapter = AsteroidsListAdapter(AsteroidsListAdapter.AsteroidsItemListener {
+            //Toast.makeText(context, "Test: ${it.id}", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
         })
 
-        binding.viewModel = viewModel
+        /* Test RecyclerView */
+        val asteroidsList = arrayListOf<Asteroid>()
+        (1 .. 100).forEach {
+            asteroidsList.add(Asteroid(it.toLong(),"Asteroid $it", "2022-01-01", 0.3 + it, 0.5 + it, 1.0 + it, 100.0, ((it % 2) == 0)))
+        }
+        asteroidsListAdapter.submitList(asteroidsList)
+
+        binding.asteroidRecycler.adapter = asteroidsListAdapter
+
 
         setHasOptionsMenu(true)
 
