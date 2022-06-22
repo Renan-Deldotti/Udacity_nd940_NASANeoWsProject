876dfb98c0c3db066487015aa36a70123e4bfe15
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2022-06-22 00:14:33 -0300

    Add ViewModel

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -3,11 +3,15 @@ package com.udacity.asteroidradar.main
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.squareup.picasso.Picasso
 import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
@@ -16,7 +20,9 @@ class MainFragment : Fragment() {
     /*private val viewModel: MainViewModel by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }*/
-    private val viewModel: MainViewModel by activityViewModels()
+    private val viewModel: MainViewModel by activityViewModels {
+        MainViewModelFactory(requireActivity().application)
+    }
     private lateinit var asteroidsListAdapter : AsteroidsListAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -31,12 +37,23 @@ class MainFragment : Fragment() {
             findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
         })
 
-        /* Test RecyclerView */
+        /* Test RecyclerView
         val asteroidsList = arrayListOf<Asteroid>()
         (1 .. 100).forEach {
             asteroidsList.add(Asteroid(it.toLong(),"Asteroid $it", "2022-01-01", 0.3 + it, 0.5 + it, 1.0 + it, 100.0, ((it % 2) == 0)))
         }
-        asteroidsListAdapter.submitList(asteroidsList)
+        asteroidsListAdapter.submitList(asteroidsList)*/
+
+        viewModel.asteroids.observe(viewLifecycleOwner) { asteroidsListAdapter.submitList(it) }
+        viewModel.todayImage.observe(viewLifecycleOwner) {
+            it?.let {
+                if (Constants.IMAGE_MEDIA_TYPE == it.mediaType) {
+                    Picasso.with(context).load(it.url).into(binding.activityMainImageOfTheDay)
+                } else {
+                    Picasso.with(context).load(R.drawable.ic_broken_image).into(binding.activityMainImageOfTheDay)
+                }
+            }
+        }
 
         binding.asteroidRecycler.adapter = asteroidsListAdapter
 
--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,36 @@
 package com.udacity.asteroidradar.main
 
+import android.app.Application
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.PictureOfDay
+import com.udacity.asteroidradar.database.AsteroidDatabase
+import com.udacity.asteroidradar.database.AsteroidRepository
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : ViewModel() {
+
+    private val database = AsteroidDatabase.getInstance(application)
+    private val repository = AsteroidRepository(database)
+
+    init {
+        viewModelScope.launch {
+            try {
+                repository.fetchAsteroidFromAPI()
+                repository.fetchTodayPictureFromAPI()
+            } catch (e: Exception) {
+                Log.e("MainViewModel", "Exception: ${e.message}")
+                Toast.makeText(application.applicationContext, "NW ISSUE", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    val asteroids: LiveData<List<Asteroid>> = repository.allAsteroidsFromWeek
+
+    val todayImage: LiveData<PictureOfDay> = repository.todayImage
 }
